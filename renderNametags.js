a7702dc07cb9d47f9a25e226ba9d83278adb1850
gwin333 <yang.290@gmail.com>
2018-11-10 13:52:42 -0600

    Nametags & partially Students

--- renderNametags.js
+++ renderNametags.js
@@ -1,16 +1,15 @@
-function createSingleNameTag() {
+function createSingleNameTag(nametag) {
     return `
-    <div class= "text-center mt-5" style= "width= 350px;
+    <div class= "text-center mt-5" style= "width: 330px; height: 180px; border: 2px solid #000; border-radius: 5px;">
+        <h2 style= "font-weight: bold; color: #fff; background-color:blue; padding:8px;">Hello, my name is:</h1>
+        <h3 style= "font-variant: small-caps; padding: 25px; text-align: auto">${nametag}</h3>
+    </div>
     `
 }
 
 
 function renderNametags(nametags) {
-    return `
-        <div class="text-center mt-5">
-            <div style= "
-        </div>
-    `
+    return nametags.map(createSingleNameTag).join('')
 }
 
 function nametags() {
--- renderStudents.js
+++ renderStudents.js
@@ -1,12 +1,33 @@
-
-function renderStudents(students) {
-    return `
-        <div class="text-center mt-5">
-            <code>${JSON.stringify(students)}</code>
-        </div>
+function singleStudent (student) {
+  return ` 
+    <div style= "${attendanceColor(student.isPresent)}; border: 1px solid #000; width: 180px; height: 70px;">
+        <h3 style= "text-align: center;">${student.name}</h4>
+        <h5 style= "font-weight: bold; text-align: center;">${attendance(student.isPresent)}</h5>
+    </div>
     `
 }
 
+function attendance (value) {
+  if (value === true) {
+    return 'Present'
+  } else if (value === false) {
+    return 'Absent'
+  }
+}
+
+function attendanceColor (value) {
+  if (value === true) {
+    return 'background-color: #99FF66'
+  } else if (value === false) {
+    return 'background-color: #CC3333'
+  }
+}
+
+function renderStudents (students) { 
+  return students.map(singleStudent).join('<br>')
+}
+
+/* <code>${JSON.stringify(students)}</code> LOOK THIS UP TO KNOW HOW IT WORKS */
 function students() {
     var content = document.getElementById('content');
 
@@ -33,5 +54,5 @@ function students() {
         }
     ]
 
-    content.innerHTML = renderStudents(studentsAbstraction);
+    content.innerHTML = `<div style = 'border: 1px solid #000'><h1 style = 'text-align: center;'>Roll Call!</h1></div>` + renderStudents(studentsAbstraction);
 }
